748a1f9865be920ee1df32fcd7b2a458258eb3c0
Nodesz-Dev <harleykempita@gmail.com>
2025-06-30 12:10:31 +1000

    Added try:except:finally blocks to the main functions of get_files_info and get_file_contents

--- functions/get_file_contents.py
+++ functions/get_file_contents.py
@@ -4,25 +4,26 @@ MAX_CHARS = 10000
 
 
 def get_file_content(working_directory, file_path):
-    
-    absolute_working_path = os.path.abspath(working_directory)
-    joined_file = os.path.join(working_directory, file_path)
-   # print(joined_file)
-    absolute_file_path = os.path.abspath(joined_file)
-   # print(absolute_file_path)
-    file_content_string = []
+    try:
+        absolute_working_path = os.path.abspath(working_directory)
+        joined_file = os.path.join(working_directory, file_path)
+        absolute_file_path = os.path.abspath(joined_file)
+        file_content_string = []
 
-    if not absolute_file_path.startswith(absolute_working_path):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.isfile(absolute_file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    with open(absolute_file_path, "r") as f:
-        file_content_string.append(f.read(MAX_CHARS))
-        if len(f.read()) > MAX_CHARS:
-            file_content_string.append(f'[...File "{file_path}" truncated at 10000 characters]') 
+        if not absolute_file_path.startswith(absolute_working_path):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        if not os.path.isfile(absolute_file_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(absolute_file_path, "r") as f:
+            file_content_string.append(f.read(MAX_CHARS))
+            if len(f.read()) > MAX_CHARS:
+                file_content_string.append(f'[...File "{file_path}" truncated at 10000 characters]') 
 
-    return "\n".join(file_content_string)
-
+        return "\n".join(file_content_string)
+    except Exception as error:
+        return f"Error: {error}"
+    finally: 
+        return f"Error: Something went wrong"
     
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,30 +2,35 @@ import os.path
 
 def get_files_info(working_directory, directory=None):
 
-    if directory.startswith("/"):
-        return f"Error: {directory} is an absolute path, cannot pass in an absolute path as second arguement"
+    try:
+        if directory.startswith("/"):
+            return f"Error: {directory} is an absolute path, cannot pass in an absolute path as second arguement"
 
-    absolute_working_directory = os.path.abspath(working_directory)
-    absolute_directory = ""
-    if directory == ".":
-        absolute_directory = absolute_working_directory
-    else:
-        absolute_directory = os.path.join(absolute_working_directory, directory)
-    
-    if not os.path.exists(absolute_directory) or directory.startswith(".."):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        absolute_working_directory = os.path.abspath(working_directory)
+        absolute_directory = ""
+        if directory == ".":
+            absolute_directory = absolute_working_directory
+        else:
+            absolute_directory = os.path.join(absolute_working_directory, directory)
+        
+        if not os.path.exists(absolute_directory) or directory.startswith(".."):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(absolute_directory):
-        return f'Error: "{directory}" is not a directory'
-    
-    list_of_file_in_directory = os.listdir(absolute_directory)
-    list_of_file_info = []
+        if not os.path.isdir(absolute_directory):
+            return f'Error: "{directory}" is not a directory'
+        
+        list_of_file_in_directory = os.listdir(absolute_directory)
+        list_of_file_info = []
 
-    for item in list_of_file_in_directory:
-        item_size = os.path.getsize(os.path.join(absolute_directory, item))
-        item_isdir = os.path.isdir(os.path.join(absolute_directory, item))
-        item_info_string = f"- {item}: file size={item_size} bytes, is_dir={item_isdir}"
+        for item in list_of_file_in_directory:
+            item_size = os.path.getsize(os.path.join(absolute_directory, item))
+            item_isdir = os.path.isdir(os.path.join(absolute_directory, item))
+            item_info_string = f"- {item}: file size={item_size} bytes, is_dir={item_isdir}"
 
-        list_of_file_info.append(item_info_string)
+            list_of_file_info.append(item_info_string)
 
-    return "\n".join(list_of_file_info)
+        return "\n".join(list_of_file_info)
+    except Exception as error:
+        return f"Error: {error}"
+    finally:
+        return f"Error: Something went wrong"
